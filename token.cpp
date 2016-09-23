9a9f0fe28a192e8f8fb846bb68cca0c6851461ed
DanteHargrow <dantehargrow@gmail.com>
2016-09-23 00:55:41 -0500

    almost complete evaluateOp
    Need to add >,< and need to finish && operators

--- token.cpp
+++ token.cpp
@@ -1,22 +1,113 @@
 #include <iostream>
+#include <cmath>
 #include <stack>
 using namesspace std; 
 
-int evaluateOp(char operator,int numberStack, char operatorStack)
+int evaluateOp(char theOperator,int numberStack, char operatorStack)
 {
 	int leftValue = numberStack.pop();
 	int rightValue = numberStack.pop();
 	int result;
-	switch(operator)
+	switch(theOperator)
 	{
-		case '+':
-		result = leftValue + rightValue;
-		return result;
-		break;
-		case '-':
-		result = leftValue - rightValue;
-		return result;
-		break;
+		case '+'://adds the two number and returns the value
+			result = leftValue + rightValue;
+			return result;
+			break;
+		case '-'://subtracts the two number and returns the value
+			result = leftValue - rightValue;
+			return result;
+			break;
+		case '/'://divides the two number and returns the value
+			result = leftValue / rightValue;
+			return result;
+			break;
+		case '*'://multiplies the two number and returns the value
+			result = leftValue * rightValue;
+			return result;
+			break;
+		case'^'://exponent
+			result = pow(leftValue, rightValue);
+			return result;
+			break;
+		case'%'://mod operator
+			result = leftValue % rightValue;
+			return result;
+			break;
+		case'!'://logical not
+			result = 0; //false
+			return result;
+			break;
+		case'A'://increments the left value by one ++
+			leftValue++;
+			return leftValue;
+			break;
+		case'B'://decrements the left value by one --
+			leftValue--;
+			return leftValue;
+			break;
+		case'C'://checks to see if two values are equal ==
+			if (leftValue == rightValue)//if they're equal return 1 for true
+			{
+				result = 1;//true
+				return result;
+			}
+			else//else return 0 for false
+			{
+				result = 0;//false
+				return result;
+			}
+			break;
+		case'D'://checks to see if left value is not equal to right value !=
+			if (leftValue != rightValue)//returns true(1) if they are not equal
+			{
+				result = 1;//true
+				return result;
+			}
+			else//if they are equal returns false(0)
+			{
+				result = 0;//false
+				return result;
+			}
+			break;
+		case'E'://logical and operator &&
+			break;
+		case'F'://logical or operator ||
+			result = 1;//true
+			return result;
+			break;
+		case'G'://greater than or equal
+			if (leftValue >= rightValue)
+			{
+				result = 1;//true
+				return result;
+			}
+			else
+			{
+				result = 0;//false
+				return result;
+			}
+			break;
+		case'H'://less than or equal
+			if (leftValue <= rightValue)
+
+			{
+				result = 1;//true
+				return result;
+			}
+			else
+			{
+				result = 0;//false
+				return result;
+			}
+			break;
+		case'I'://negative
+			result = leftValue * -1;
+			return result;
+			break;
+			
+
+				
 
 	}
 }
