9a27a355d4043fb4d49467446a51ed1f29135d54
danieln5004 <danieln816@yahoo.com>
2016-09-25 16:32:19 -0500

    more errors
    more errors added

--- errorsMenu.cpp
+++ errorsMenu.cpp
@@ -4,18 +4,21 @@ using namespace std;
 int main (){
 	std::std::map<char, string> map;
 
-	errorMap[")"] = /*" a closing parenthese"*/;
-	errorMap["^"] = /*" "*/;
-	errorMap["*"] = /*" "*/;
-	errorMap["/"] = /*" "*/;
-	errorMap["%"] = /*" "*/;
-	errorMap[">"] = /*" "*/;
-	errorMap["<"] = /*" "*/;
-	errorMap[">="] = /*" "*/;
-	errorMap["<="] = /*" "*/;
-	errorMap["=="] = /*" "*/;
-	errorMap["!="] = /*" "*/;
-	errorMap["&&"] = /*" "*/;
-	errorMap["||"] = /*" "*/;
+	errorMap[")"] = " a closing parenthese";
+	errorMap["^"] = " a carrot operator";
+	errorMap["*"] = " a multipication operator";
+	errorMap["/"] = " a dividson operator";
+	errorMap["%"] = " a mod operator ";
+	errorMap[">"] = " a comparison operator";
+	errorMap["<"] = " a comparison operator";
+	errorMap[">="] = " a comparison operator";
+	errorMap["<="] = " a comparison operator";
+	errorMap["=="] = " a comparison operator";
+	errorMap["!="] = " a comparison operator";
+	errorMap["&&"] = " a comparison operator";
+	errorMap["||"] = " a comparison operator";
+
+
+	cout << "Expression cannot start with " << errorMap[] << " @ char: 0"
 
 }
