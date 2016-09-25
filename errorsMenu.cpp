de52e2d97d2d6bb6f4811f7b40944bf244633735
danieln5004 <danieln816@yahoo.com>
2016-09-25 14:39:37 -0500

    errors
    still working on it

---
+++ errorsMenu.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+int main (){
+	std::std::map<char, string> map;
+
+	errorMap[")"] = /*" a closing parenthese"*/;
+	errorMap["^"] = /*" "*/;
+	errorMap["*"] = /*" "*/;
+	errorMap["/"] = /*" "*/;
+	errorMap["%"] = /*" "*/;
+	errorMap[">"] = /*" "*/;
+	errorMap["<"] = /*" "*/;
+	errorMap[">="] = /*" "*/;
+	errorMap["<="] = /*" "*/;
+	errorMap["=="] = /*" "*/;
+	errorMap["!="] = /*" "*/;
+	errorMap["&&"] = /*" "*/;
+	errorMap["||"] = /*" "*/;
+
+}
