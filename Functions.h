0fe34935fd8b0caae786c2aaef22b136e485b9f4
DanteHargrow <dantehargrow@gmail.com>
2016-09-25 21:19:05 -0500

    Class
    blah

--- Functions.h
+++ Functions.h
@@ -1,1 +1,21 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+class infixParser
+{
+public:
+	infixParser();
+	
+	
+
+private:
+	string removeSpaces(string str);
+	int precedenceOfOperator(char op);
+	string reduceExpression(string str);
+	int calculate(int number1, char op, int number2 = -1);
+	double processedAnswer(string expn);
+	void isBalcanced(string str);
+};
 
