b2cf12651a7068af6377643b5ed7a9f203d03df4
danieln5004 <danieln816@yahoo.com>
2016-09-25 21:01:25 -0500

    errors2

--- errorsMenu.cpp
+++ errorsMenu.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main (){
 	std::std::map<char, string> map;
 
+
+	//errors at the beginning of a expression
 	errorMap[")"] = " a closing parenthese";
 	errorMap["^"] = " a carrot operator";
 	errorMap["*"] = " a multipication operator";
@@ -18,6 +20,10 @@ int main (){
 	errorMap["&&"] = " a comparison operator";
 	errorMap["||"] = " a comparison operator";
 
+
+	//errors with in the expression 
+	errorMap[" "] = " a space between numbers, unable to read expression for evaluation";
+	errorMap[""]
 
 	cout << "Expression cannot start with " << errorMap[] << " @ char: 0"
 
